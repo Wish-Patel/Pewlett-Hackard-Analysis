c0e0303696313c27ce9752dc4e2c997800d5e95b
Wish Patel <patelnovi@gmail.com>
2019-11-17 13:50:08 -0800

    retirement query

--- schema.sql
+++ schema.sql
@@ -1,5 +1,5 @@
 -- Creating tables for PH-EmployeeDB
-SELECT * FROM departments;
+SELECT * FROM titles;
 CREATE TABLE departments (
      dept_no VARCHAR(4) NOT NULL,
      dept_name VARCHAR(40) NOT NULL,
@@ -37,8 +37,8 @@ CREATE TABLE salaries (
 );
 
 CREATE TABLE dept_employees (
+emp_no INT NOT NULL,
 	dept_no VARCHAR(4) NOT NULL,
-	emp_no INT NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
@@ -48,10 +48,8 @@ FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 
 CREATE TABLE titles (
   emp_no INT NOT NULL,
-  dept_no VARCHAR(4) NOT NULL,
+  title VARCHAR(40) NOT NULL,
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
-FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-	PRIMARY KEY (emp_no, dept_no)
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
